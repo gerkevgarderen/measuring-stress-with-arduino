5c56d1e44593d887f9c546b170b6d60db1d9d23a
Gerke van Garderen <info@gerkevangarderen.nl>
2023-05-03 20:25:41 +0200

    Cleanup; improve documentation

--- Heartbeat/Heartbeat.ino
+++ Heartbeat/Heartbeat.ino
@@ -2,17 +2,17 @@
  *  For the original Pulse Sensor Amped code and license of said code, please see: https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/LICENSE
  */
 
- /*
-  * This edited and extended version of code was written by Gerke van Garderen, https://gerkevangarderen.nl/
-  * It was written as an extension of beformentioned code, to add stress sensing through monitoring of Heart Rate Variability (HRV)
-  * 
-  * Note: because the sensor code makes use of Timer2, pins 3 and 11 lose PWM-functionality.
-  */
+/*
+ * This edited and extended version of code was written by Gerke van Garderen, https://gerkevangarderen.nl/
+ * It was written as an extension of beformentioned code, to add stress sensing through monitoring of Heart Rate Variability (HRV)
+ * 
+ * Note: because the sensor code makes use of Timer2, pins 3 and 11 lose PWM-functionality.
+ */
 
 #define LOG_OUT 1 // use the log output function
-#define FHT_N 32 // set to 256 point fht
+#define FHT_N 32  // Set number of data points the FHT will use
 
-#include <FHT.h> // include the library for Fourier transform
+#include <FHT.h>  // include the library for Fourier transform
 
 #define vibrationPin 5
 boolean vibing = true; //checks whether vibration is currently on
@@ -26,9 +26,8 @@ float LFHF,LFHFOld;
 
 
 //  Variables
-//int sensorVcc = 12;
-int pulsePin = A0;                 // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 13;                // pin to blink led at each beat
+int pulsePin = A0;                  // Pulse Sensor purple wire connected to analog pin 0
+int blinkPin = 13;                  // pin to blink led at each beat
 
 // Volatile Variables, used in the interrupt service routine!
 volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
@@ -42,31 +41,18 @@ int dataCount = 0;
 boolean once = false;
 
 void setup(){
- // pinMode(sensorVcc,OUTPUT);
- // digitalWrite(sensorVcc,HIGH);
-  
   pinMode(blinkPin,OUTPUT);         // pin that will blink to your heartbeat!
 
   Serial.begin(115200);             // we agree to talk fast!
   interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
-
-
 }
 
 
-//  Where the Magic Happens
 void loop(){
-  
-    //serialOutput() ;       
-    
   if (QS == true){                      // A Heartbeat Was Found, BPM and IBI have been Determined. Quantified Self "QS" is set to true when arduino finds a heartbeat
         serialOutputWhenBeatHappens();  // A Beat Happened, Output that to serial. 
         Fourier();                      // Run the Fourier code to calculate HF/LF    
         QS = false;                     // reset the Quantified Self flag for next time    
   }
-  delay(10);                             //  take a break
+  delay(10);                            //  take a break
 }
-
-
-
-
